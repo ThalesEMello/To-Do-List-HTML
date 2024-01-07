03c701e8c8cb5c2dc1f08f11a343f539bc8af6ee
Thales E Mello <thales04mello@gmail.com>
2024-01-07 09:41:26 -0300

    Add new function "Show Task"

--- scripts.js
+++ scripts.js
@@ -9,4 +9,17 @@ function AddNewTask() {
   console.log(MyItensList);
 }
 
+function ShowTasks() {
+  let NewLi = "";
+  MyItensList.forEach((Task) => {
+    NewLi = `
+    <li class="task">
+      <img src="img/check.png" alt="check-mark" />
+      <p>Concluir o JS do Task Manager</p>
+      <img src="img/delete-button.png" alt="trash" />
+    </li>
+    `;
+  });
+}
+
 button.addEventListener("click", AddNewTask);
